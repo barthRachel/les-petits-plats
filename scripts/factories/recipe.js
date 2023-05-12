bab614a6c51a304c5ca7a1ad7a0f991c97f86bfa
Rachel Barthélery <rachel-barth@live.fr>
2023-05-12 21:25:16 +0200

    Début affichage recettes

---
+++ scripts/factories/recipe.js
@@ -0,0 +1,9 @@
+function recipeFactory(data) {
+    const {name, servings, ingredients, time, description, appliance, ustensils} = data;
+
+    function getRecipeCardDOM() {
+        //card recipe à coder
+    }
+
+    return { getRecipeCardDOM }
+}
